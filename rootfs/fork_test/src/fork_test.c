c3e3772482119ca264152b542ce0b445ee8e9306
Stefan Liao <stefan.liao.cs11@nycu.edu.tw>
2023-05-04 14:31:43 +0800

    feat. [USR_Prog] Available user program to pass fork test. TODO: Make infrastructure in user prog (atoi, hex...).

--- rootfs/fork_test/src/fork_test.c
+++ rootfs/fork_test/src/fork_test.c
@@ -3,26 +3,30 @@
 #include "uart.h"
 
 int main(void) {
-    init_printf(0, putc);
+    // init_printf(0, putc);
     
-    printf("\n[fork_test]Fork Test, pid %d\n", getpid());
+    // printf("\n[fork_test]Fork Test, pid %d\n", getpid());
+    uartwrite("\n[fork_test]Fork Test, pid", 27);
+
     int cnt = 1;
     int ret = 0;
-    printf("pid: %d, cnt: %d, cnt_adress: 0x%x\n", getpid(), cnt, &cnt);
+    // printf("pid: %d, cnt: %d, cnt_adress: 0x%x\n", getpid(), cnt, &cnt);
+    uartwrite("(Outer) pid: %d, cnt: %d, cnt_adress: 0x%x\n", 44);
+
     if ((ret = fork()) == 0) { // child
-        printf("pid: %d, cnt: %d, cnt_adress: 0x%x\n", getpid(), cnt, &cnt);
+        uartwrite("(Inner 1) pid: %d, cnt: %d, cnt_adress: 0x%x\n", 46);
         ++cnt;
         fork();
         while (cnt < 5) {
-            printf("pid: %d, cnt: %d, cnt_adress: 0x%x\n", getpid(), cnt, &cnt);
+            uartwrite("(Inner 2) pid: %d, cnt: %d, cnt_adress: 0x%x\n",46);
             for(int i = 0;i < 100000;i++);
-            ++cnt;
+                ++cnt;
         }
     } else {
-        printf("parent here, pid %d, child %d\n", getpid(), ret);
+        uartwrite("parent here, pid %d, child %d\n",31);
     }
     // dumpTasksState();
-    printf("[exit] Task%d\n", getpid());
+    uartwrite("[exit] Task%d\n", 15);
     exit();
     return 0;
 }
